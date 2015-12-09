27d2aaf42c71056eeb1dc4ecf60675dc5bf3320a
lukasz18dg <lukasz18dg@gmail.com>
2015-12-09 12:28:52 +0100

    19.02.2013r - 16:53

--- localhost.sql
+++ localhost.sql
@@ -1,10 +1,33 @@
-
+-- phpMyAdmin SQL Dump
+-- version 3.5.6
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Czas wygenerowania: 19 Lut 2013, 15:53
+-- Wersja serwera: 5.5.29
+-- Wersja PHP: 5.2.17
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Baza danych: `projekt`
+--
 CREATE DATABASE `projekt` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
 USE `projekt`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `odpowiedzi_uzytkownika`
+--
+
 CREATE TABLE IF NOT EXISTS `odpowiedzi_uzytkownika` (
   `id_odpowiedzi_uzytkownika` int(255) NOT NULL AUTO_INCREMENT,
   `id_uzytkownika` int(255) NOT NULL,
@@ -15,11 +38,30 @@ CREATE TABLE IF NOT EXISTS `odpowiedzi_uzytkownika` (
   PRIMARY KEY (`id_odpowiedzi_uzytkownika`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `przedmioty`
+--
+
 CREATE TABLE IF NOT EXISTS `przedmioty` (
   `id_przedmiotu` int(255) NOT NULL AUTO_INCREMENT,
-  `przedmiot` int(255) NOT NULL,
+  `przedmiot` tinytext COLLATE utf8_polish_ci NOT NULL,
   PRIMARY KEY (`id_przedmiotu`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=2 ;
+
+--
+-- Zrzut danych tabeli `przedmioty`
+--
+
+INSERT INTO `przedmioty` (`id_przedmiotu`, `przedmiot`) VALUES
+(1, 'Brak kategori');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pytania_i_odpowiedzi`
+--
 
 CREATE TABLE IF NOT EXISTS `pytania_i_odpowiedzi` (
   `id_pytania` int(255) NOT NULL AUTO_INCREMENT,
@@ -31,6 +73,12 @@ CREATE TABLE IF NOT EXISTS `pytania_i_odpowiedzi` (
   PRIMARY KEY (`id_pytania`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `test`
+--
+
 CREATE TABLE IF NOT EXISTS `test` (
   `id_testu` int(255) NOT NULL AUTO_INCREMENT,
   `tworca` int(255) NOT NULL,
@@ -39,6 +87,12 @@ CREATE TABLE IF NOT EXISTS `test` (
   `dozwolony` mediumtext COLLATE utf8_polish_ci NOT NULL,
   PRIMARY KEY (`id_testu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uzytkownicy`
+--
 
 CREATE TABLE IF NOT EXISTS `uzytkownicy` (
   `id_uzytkownika` int(255) NOT NULL AUTO_INCREMENT,
@@ -51,7 +105,15 @@ CREATE TABLE IF NOT EXISTS `uzytkownicy` (
   PRIMARY KEY (`id_uzytkownika`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=4 ;
 
+--
+-- Zrzut danych tabeli `uzytkownicy`
+--
+
 INSERT INTO `uzytkownicy` (`id_uzytkownika`, `login`, `haslo`, `moc`, `email`, `klasa`, `litera`) VALUES
 (1, 'Admin', '7ae7a24fc34645787cd4d447e661a1c47ec7e52b', 'A', NULL, NULL, NULL),
 (2, 'Nauczyciel', '7ae7a24fc34645787cd4d447e661a1c47ec7e52b', 'M', NULL, NULL, NULL),
 (3, 'Uczen', '7ae7a24fc34645787cd4d447e661a1c47ec7e52b', 'S', NULL, '4', 'c');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
